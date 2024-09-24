9aeb8b060d8d101813778464fffee39bc37aa33a
sushanthreddy29 <spapa3@unh.newhaven.edu>
2024-09-24 00:14:11 -0400

    Added function to save info to database

--- app/src/main/java/com/unh/icebreaker_sushanth_f24/MainActivity.kt
+++ app/src/main/java/com/unh/icebreaker_sushanth_f24/MainActivity.kt
@@ -9,10 +9,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.unh.icebreaker_sushanth_f24.databinding.ActivityMainBinding
 import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val className = "android-fall24"
+    private val db = Firebase.firestore
+    private var TAG = "IcebreakerF24"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +34,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    
     private fun writeStudentToFirebase(){
-        val firstName = binding.txtFirstName.text
-        val lastName = binding.txtLastName.text
-        val prefName = binding.txtPrefName.text
-        val answer = binding.txtAnswer.text
+        val firstName = binding.txtFirstName
+        val lastName = binding.txtLastName
+        val prefName = binding.txtPrefName
+        val answer = binding.txtAnswer
 
-        Log.d("IcebreakerF24", "Variables: $firstName $lastName $prefName $answer")
+        Log.d(TAG, "Variables: $firstName $lastName $prefName $answer")
+
+        val student = hashMapOf(
+            "firstName" to firstName.text.toString(),
+            "lastName" to lastName.text.toString(),
+            "prefName" to prefName.text.toString(),
+            "answer" to answer.text.toString(),
+            "class" to className,
+            "question" to binding.txtQuestion.text.toString()
+        )
+
+        db.collection("students")
+            .add(student)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG,"DocumentSnapshot written successfully with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error adding document", exception)
+            }
+
+        firstName.setText("")
+        lastName.setText("")
+        prefName.setText("")
+        answer.setText("")
 
     }
 }
